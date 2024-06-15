f7426274f34de77c95772df58ea7547ff36a814a
mawethuN <2128875@students.wits.ac.za>
2024-06-15 05:07:51 +0200

    tjo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouterRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Theme from './pages/Theme';
@@ -12,7 +12,7 @@ import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <div>
-        <BrowserRouter>
+        <HashRouter>
         <Navbar />
           <Routes>
             <Route index element={<Home />} />
@@ -24,7 +24,7 @@ export default function App() {
             <Route path="/design" element={<Design />} />
             <Route path="*" element={<NoPage />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
   )
 }
